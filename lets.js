17c14eb1f3bf56674cfca37d5a11df2012d5a667
mansi1426 <35780904+mansi1426@users.noreply.github.com>
2018-03-27 18:57:28 +0530

    Add files via upload

---
+++ lets.js
@@ -0,0 +1,73 @@
+var text=[
+	"Provide Proper Tax Solutions", "Tax Strategy & Management", "Tax Compliance & Reporting"
+];
+var end=[
+	"Affordable tax advisory services for your benefit", "Improving the effectiveness of your tax strategy", "Ensuring that tax and process risks are minimized"
+];
+var k;
+var l;
+var h=document.querySelector(".content h1");
+var y=document.querySelector(".content h4");
+h.textContent=text[0];
+y.textContent=end[0];
+var i=1;
+function animate(){
+		h.textContent=text[i];
+		y.textContent=end[i];
+		i++;
+		if(i==3)
+			{i=0;}
+	}
+var value=setInterval(animate,3000);
+var t=document.querySelector(".left");
+t.addEventListener("click", function(){
+	clearInterval(value);
+	clearInterval(k);
+	clearInterval(l);
+	if(i==0)
+		i=1;
+	else if (i==1)
+		i=2;
+	else i=0;
+	h.textContent=text[i];
+y.textContent=end[i];
+i++;
+if(i==3)
+{i=0;}
+	 k=setInterval(animate,3000);
+});
+var g=document.querySelector(".right");
+g.addEventListener("click", function(){
+	clearInterval(value);
+	clearInterval(l);
+	clearInterval(k);
+if(i==3)
+	i=0;
+h.textContent=text[i];
+y.textContent=end[i];
+i++;
+if(i==3)
+	i=0;
+l=setInterval(animate,3000);
+});
+
+
+
+
+
+$(document).ready(function(){
+
+	$('.owl-two').owlCarousel({
+	  animateOut: 'slideOutLeft',
+	  animateIn: 'slideInRight',
+	  items: 1,
+	  margin: 30,
+	  stagePadding: 30,
+	  smartSpeed: 400,
+	  autoplay:true,
+	  autoplayTimeout:3000,
+	  autoplayHoverPause:true,
+	  loop: true,
+	  nav:true
+	});      
+  });
